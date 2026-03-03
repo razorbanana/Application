3f1e85e012c797c7779db4dd04485f24cf1874a3
Stanislav Stefiuk <stanislav.stefyuk2@gmail.com>
2026-03-03 15:14:52 +0200

    feat: error handling filter

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cors from "cors"
+import { CatchEverythingFilter } from './utils/filters/catch-everything.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors())
+  app.useGlobalFilters(new CatchEverythingFilter(app.get(HttpAdapterHost)))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ apps/backend/src/utils/filters/catch-everything.filter.ts
@@ -0,0 +1,28 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from "@nestjs/common";
+import { HttpAdapterHost } from "@nestjs/core";
+import { timestamp } from "rxjs";
+
+@Catch()
+export class CatchEverythingFilter implements ExceptionFilter {
+    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+    catch(exception: any, host: ArgumentsHost): void {
+        const {httpAdapter} = this.httpAdapterHost;
+        const ctx = host.switchToHttp();
+        const httpStatus = 
+            exception instanceof HttpException
+            ? exception.getStatus()
+            : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        console.error(exception);
+
+        const responseBody = {
+            statusCode: httpStatus,
+            timestamp: new Date().toISOString(),
+            path: httpAdapter.getRequestUrl(ctx.getRequest()),
+            message: exception instanceof HttpException ? exception.message : 'Internal server error',
+        };
+
+        httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus)
+    }
+}
