6ee546f989e9f6c113b7569cbc1e95fbd5dbe7d6
Stanislav Stefiuk <stanislav.stefyuk2@gmail.com>
2026-03-13 13:09:54 +0200

    feat: tag chips are displayed in calendar

--- apps/frontend/src/components/commonComponents/TagChip.tsx
+++ apps/frontend/src/components/commonComponents/TagChip.tsx
@@ -3,14 +3,14 @@ import { tagConfig } from "../../utils/config/tagConfig"
 
 type TagChipProps = {
     tag: TagName,
-    mode: 'icons' | 'full'
+    mode: 'icons' | 'full' | 'calendar'
 }
 
 export default function TagChip({tag, mode}: TagChipProps){
     const config = tagConfig[tag]
     const Icon = config.icon
     return (
-        <div className={`flex items-center gap-1 px-2 py-1 rounded-full text-xs w-fit ${config.color}`}>
+        <div className={`inline-flex items-center gap-1 px-2 py-1 rounded-full text-xs w-fit ${config.color}`}>
             <Icon size={12} />
             {mode == 'full' && tag.toUpperCase()}
         </div>
--- apps/frontend/src/components/commonComponents/Tags.tsx
+++ apps/frontend/src/components/commonComponents/Tags.tsx
@@ -3,13 +3,13 @@ import TagChip from "./TagChip"
 
 type EventCardTagsProps = {
     tags: TagType[] | undefined,
-    mode?: 'icons' | 'full'
+    mode?: 'icons' | 'full' | 'calendar'
 }
 
 export default function Tags({tags, mode='full'}:EventCardTagsProps){
 
     return (
-        <div className="py-1">
+        <div className={`py-1 inline-flex gap-1  ${mode === 'calendar' && "absolute right-0 top-0 mr-2 mt-1"}`}>
             {tags && tags.map(t => <TagChip key={t.id} tag={t.name} mode={mode}/>)}
         </div>
     )
